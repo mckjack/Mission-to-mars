ffbe21ed2507a06afecc6772f604a02d782c5432
Jack McKinnon <jackmckinnon13@hotmail.com>
2022-05-15 22:56:41 -0400

    Adding Module 10 Challenge

--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemisphere_image_info": hemisphere_image(browser)
     }
      # Stop webdriver and return data
     browser.quit()
@@ -91,6 +92,52 @@ def mars_facts():
 
 
     return df.to_html()
+
+## DELIVERABLE 2
+def hemisphere_image(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(url)
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+# Parse the html with soup
+    html = browser.html
+    home_page_soup = soup(html, 'html.parser')
+
+        
+# Add try/except for error handling
+    try:
+        count_pics= len(home_page_soup.select("div.item"))
+# for loop
+        for i in range(count_pics):
+    # Create an empty dict to hold the search results
+            results = {}
+            link_image = home_page_soup.select("div.description a")[i].get('href')
+            browser.visit(f'https://astrogeology.usgs.gov{link_image}')
+    
+    # Parse the new html page with soup
+            html = browser.html
+            sample_image_soup = soup(html, 'html.parser')
+    # Get the full image link
+            img_url = sample_image_soup.select_one("div.downloads ul li a").get('href')
+    # Get the full image title
+            img_title = sample_image_soup.select_one("h2.title").get_text()
+            results = {
+           'img_url': img_url,
+            'title': img_title}
+    
+    # Append results 
+            hemisphere_image_urls.append(results)
+    
+    # Return to main page
+            browser.back()              
+    except BaseException:
+        return None
+    
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
